609795f34024e917aae7860b852ead152251fc9e
Leonardo Nascimento <leo.nascimentoo02@gmail.com>
2021-09-22 23:21:29 -0300

    ajuste para java 8

--- src/test/java/TddTeste/PalindromoTest.java
+++ src/test/java/TddTeste/PalindromoTest.java
@@ -1,10 +1,12 @@
 package TddTeste;
 
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import principal.Palindromo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PalindromoTest {
 
@@ -12,10 +14,8 @@ public class PalindromoTest {
 
     @Test
     @DisplayName("Verifica se o palindromo é verdadeiro")
-    public void palindromoVerdadeiro(){
-        List<String> listaDePalindromo = List.of("Rotator", "bob", "madam",//
-                                    "mAlAyAlam", "1", "Able was I, ere I saw Elba",//
-                                         "Madam I’m Adam","Step on no pets.", "Top spot!","02/02/2020");
+    public void palindromoVerdadeiro() {
+        List<String> listaDePalindromo = listaPositiva();
         listaDePalindromo.forEach(item -> {
             Assertions.assertTrue(palindromo.ehPalindromo(item));
             System.out.printf("O texto: %s é um palíndromo.%n", item);
@@ -24,12 +24,37 @@ public class PalindromoTest {
 
     @Test
     @DisplayName("Verifica se o palidromo eh falso")
-    public void palindromoFalso(){
-        List<String> listaDePalindromo = List.of("xyz", "elephant", "Country",//
-                                    "Top . post!", "Wonderful-fool", "Wild imagination!");
+    public void palindromoFalso() {
+        List<String> listaDePalindromo = listaNegativa();
         listaDePalindromo.forEach(item -> {
             Assertions.assertFalse(palindromo.ehPalindromo(item));
             System.out.printf("O texto: %s não é um palíndromo.%n", item);
         });
     }
+
+    private List<String> listaPositiva() {
+        List<String> listaDePalindromo = new ArrayList<>();
+        listaDePalindromo.add("Rotator");
+        listaDePalindromo.add("bob");
+        listaDePalindromo.add("madam");
+        listaDePalindromo.add("mAlAyAlam");
+        listaDePalindromo.add("1");
+        listaDePalindromo.add("Able was I, ere I saw Elba");
+        listaDePalindromo.add("Madam I’m Adam");
+        listaDePalindromo.add("Top spot!");
+        listaDePalindromo.add("02/02/2020");
+        return listaDePalindromo;
+    }
+
+    private List<String> listaNegativa() {
+        List<String> listaDePalindromo = new ArrayList<>();
+        listaDePalindromo.add("xyz");
+        listaDePalindromo.add("elephant");
+        listaDePalindromo.add("Country");
+        listaDePalindromo.add("Top . post!");
+        listaDePalindromo.add("Wonderful-fool");
+        listaDePalindromo.add("Wild imagination!");
+        return listaDePalindromo;
+    }
+
 }
